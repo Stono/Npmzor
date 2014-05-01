9fc21db97a3b4826737ef3e8c4cd53a22517e4cd
Karl Stoney <karlstoney@googlemail.com>
2014-05-01 19:32:08 +0100

    Some more index related tests and code

--- test/unit/registryManager.js
+++ test/unit/registryManager.js
@@ -12,7 +12,7 @@ describe('Registry Manager (registryManager)', function() {
 
   var registryManager;
   var npmRegistry;
-  var registryRoot = 'http://127.0.0.1';
+  var registryRoot = 'registry1';
 
   beforeEach(function() {
     var httpUtil    = new HttpUtil(mockConfig, http);
@@ -40,23 +40,85 @@ describe('Registry Manager (registryManager)', function() {
       });      
     });
   });
-  
-  it('Should forward index requests to the external Registry', function(done) {
-    var mockRegistry = deride.wrap(npmRegistry);
-    mockRegistry.setup.getModuleIndex.toDoThis(function(name, callback) {
-      callback(undefined, {
-        _id: 'fake-repo'
+
+  describe('Index Queries', function() {
+    
+    it('Should forward requests to the external Registry', function(done) {
+      var mockRegistry = deride.wrap(npmRegistry);
+      mockRegistry.setup.getModuleIndex.toDoThis(function(name, callback) {
+        callback(undefined, {
+          _id: 'fake-repo'
+        });
       });
-    });
-    registryManager.addRegistry(mockRegistry, function(err) {
-      assert.equal(err, undefined, err);
+      
+      registryManager.addRegistrySync(mockRegistry);    
       registryManager.getModuleIndex('fake-repo', function(err, details) {
         assert.equal(err, undefined, err);
         assert.equal(details._id, 'fake-repo');
         mockRegistry.expect.getModuleIndex.called.once();
         done();
       });
+      
     });
+    
+    it('Should not hit the second registry if the first returned a positive result', function(done) {
+      var mockRegistry1 = deride.wrap(npmRegistry);
+      mockRegistry1.setup.getModuleIndex.toDoThis(function(name, callback) {
+        callback(undefined, {
+          _id: 'fake-repo'
+        });
+      });
+      
+      var mockRegistry2 = deride.wrap(npmRegistry);
+      mockRegistry2.setup.getRoot.toReturn('registry2');
+      
+      registryManager.addRegistrySync(mockRegistry1);
+      registryManager.addRegistrySync(mockRegistry2);
+      
+      registryManager.getModuleIndex('fake-repo', function(err, details) {
+        assert.equal(err, undefined, err);
+        assert.equal(details._id, 'fake-repo');
+        mockRegistry1.expect.getModuleIndex.called.once();
+        mockRegistry2.expect.getModuleIndex.called.never();
+        done();
+      });
+    
+    });
+    
+    it('Should hit the second registry if the first returned a negative result', function(done) {
+      var mockRegistry1 = deride.wrap(npmRegistry);    
+      var mockRegistry2 = deride.wrap(npmRegistry);
+      mockRegistry1.setup.getModuleIndex.toDoThis(function(name, callback) {
+        callback(undefined, null);
+      });
+      
+      mockRegistry2.setup.getRoot.toReturn('registry2');
+      mockRegistry2.setup.getModuleIndex.toDoThis(function(name, callback) {
+        callback(undefined, {
+          _id: 'fake-repo'
+        });
+      });
+      
+      registryManager.addRegistrySync(mockRegistry1);
+      registryManager.addRegistrySync(mockRegistry2);
+      
+      registryManager.getModuleIndex('fake-repo', function(err, details) {
+        assert.equal(err, undefined, err);
+        assert.equal(details._id, 'fake-repo');
+        mockRegistry1.expect.getModuleIndex.called.once();
+        mockRegistry2.expect.getModuleIndex.called.once();
+        done();
+      });
+    
+    });
+    
+    it('Should return an error if no results were found', function(done) {
+      registryManager.getModuleIndex('fake-repo-no-results', function(err) {
+        assert.notEqual(err, undefined);
+        done();
+      });      
+    });
+    
   });
   
 });
