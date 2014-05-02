35941f94d314ca7f7054c1601d040e8523add4b7
Karl Stoney <karl.stoney@hp.com>
2014-05-02 16:27:34 +0100

    Added the proxying of TGZ files

--- test/acceptance/application.js
+++ test/acceptance/application.js
@@ -2,6 +2,10 @@
 var restler    = require('restler');
 var assert     = require('assert');
 var config     = require('../../config');
+var http       = require('http');
+var fs         = require('fs');
+var crypto     = require('../../lib/crypto');
+
 /*
  * Decided to have these tests hit the actual
  * npmjs.org registry, as then we know fo' sho'
@@ -37,6 +41,22 @@ describe('NPMZor', function() {
       assert.equal(result.version, '0.1.0');
       assert.equal(res.statusCode, 200);
       done();
+    });
+  });
+
+  it.only('Should return a module when the request is valid', function(done) {
+    var target  = '/tmp/' + Date.now().toString(12);
+    var file    = fs.createWriteStream(target);
+    var request = http.get(endPoint + '/deride/-/deride-0.1.0.tgz', function(response) {
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close(function() {
+          crypto.sha1(target, function(error, sum) {
+            assert.equal(sum, 'e00f8de0eeb0da49ae51ad926742d4ab44fc1c32');
+            done();
+          });
+        });
+      });
     });
   });
 
