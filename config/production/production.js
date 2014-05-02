d7eb507ba9339107ac3bf330aed61267636c8b87
Karl Stoney <karl.stoney@hp.com>
2014-05-02 08:40:55 +0100

    Added the ability to pull proxy config from the environment

--- config/production/production.js
+++ config/production/production.js
@@ -12,4 +12,23 @@ var ProductionConfig = {
   ]
 };
 
+var extractUrl = function(url) {
+  var urlRegex = /^(.*):\/\/([a-z\-.]+):?([0-9]+)?(.*)$/;
+  var extract  = process.env.http_proxy.match(urlRegex);
+  return {
+    hostname: extract[2],
+    port: extract[3],
+    protocol: extract[1]
+  };
+};
+
+// Now we need to detect environment proxies
+if(process.env.http_proxy) {
+  ProductionConfig.http.proxy.http = extractUrl(process.env.http_proxy);
+};
+
+if(process.env.https_proxy) {
+  ProductionConfig.http.proxy.https = extractUrl(process.env.https_proxy);
+};
+
 module.exports = ProductionConfig;
