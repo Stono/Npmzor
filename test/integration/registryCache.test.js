19f0af8c2c6b406749128ce7c2a1d36459e6b1ee
Karl Stoney <karlstoney@googlemail.com>
2014-05-04 00:13:41 +0100

    Added set + get methods to cache

--- test/integration/registryCache.test.js
+++ test/integration/registryCache.test.js
@@ -2,11 +2,15 @@
 var fs     = require('fs');
 var assert = require('assert');
 var RegistryCache = require('../../lib/registryCache').RegistryCache;
+var mockConfig    = require('../mockConfig').getNoProxyConfig();
 
 describe('NPM Registry Cache', function() {
   var registryCache;
   var pathToDbs    = __dirname + '/../../db';
   var pathToTestDb = pathToDbs + '/test';
+
+  var contents = fs.readFileSync(__dirname + '/../data/sample-requests/mkdirp').toString();
+    contents = JSON.parse(contents);
   
   var deleteFolderRecursive = function(path) {
     if( fs.existsSync(path) ) {
@@ -38,17 +42,26 @@ describe('NPM Registry Cache', function() {
   beforeEach(function() {
     var Db = require('tingodb')().Db;
     var db = new Db(pathToTestDb, {});
-    registryCache = new RegistryCache(db);
+    mockConfig.cache.timeout = 60;
+    registryCache = new RegistryCache(mockConfig, db);
   });
   
   it('Should add an index to its cache', function(done) {
-    var contents = fs.readFileSync(__dirname + '/../data/sample-requests/mkdirp').toString();
-    contents = JSON.parse(contents);
     registryCache.addIndex(contents, function(err) {
-      assert.equal(err, null, 'There was an error inserting the cache item');
+      assert.equal(err, undefined, 'There was an error inserting the cache item');
       registryCache.getIndex('mkdirp', function(err, index) {
-        assert.equal(err, null, 'There was an error reading the cache item');
-        assert.equal(index.toString(), contents.toString(), 'The cache item was not found in the cache');
+        assert.equal(JSON.stringify(index), JSON.stringify(contents), 'The cache item was not found in the cache');
+        done();
+      });
+    });
+  });
+  
+  it('Should not return an index that has passed its expiry', function(done) {
+    mockConfig.cache.timeout = 0;
+    registryCache.addIndex(contents, function(err) {
+      assert.equal(err, undefined, 'There was an error inserting the cache item');
+      registryCache.getIndex('mkdirp', function(err, index) {
+        assert.equal(index, undefined);
         done();
       });
     });
