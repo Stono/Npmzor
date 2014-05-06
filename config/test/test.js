e1219b5b5d2b1032c78926f60e8491fbf18e45c7
Karl Stoney <karlstoney@googlemail.com>
2014-05-06 12:42:07 +0100

    Fixed test config file location to be in /tmp to fix travis

--- config/test/test.js
+++ config/test/test.js
@@ -4,6 +4,15 @@ var productionConfig = require('../production/production');
 var testConfig = {
   port: 8080,
   url: 'http://127.0.0.1:8080',
+  temp: '/tmp/npmzor/temp',
+  db: '/tmp/npmzor/db/' + (process.env.ENV || 'dev'),
+  internal: {
+    tgz: '/tmp/npmzor/internal'
+  },
+  cache: {
+    // Timeout is in seconds
+    tgz: '/tmp/npmzor/cache'
+  },
   logging: {
     console: false,
     file: false
